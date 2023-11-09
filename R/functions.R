80330e4a9e6b619d3aeb38bc1cf8dc7ce883c5ad
Kaja Madsen <kajamadsen@health.sdu.dk>
2023-11-09 10:58:51 +0100

    Adding plot_distribution to functions.R

--- R/functions.R
+++ R/functions.R
@@ -12,3 +12,17 @@ descriptive_stats <-   function(data) {
     dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
     dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+
+#' Plot for basic distribution of metabolite
+#'
+#' @param dataThe lipidomics dataset
+#'
+#' @return A plot object
+
+plot_distributions <- function(data) {
+  data %>%
+    ggplot2::ggplot(ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
